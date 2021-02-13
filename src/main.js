6298935642cf88ba88e371ab8a523dddf39a63ed
vektorSpace <seetharaman.vivek@gmail.com>
2021-02-14 03:14:29 +0800

    three.js boiler plate
    Three.js template added

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'https://unpkg.com/three@0.117.0/build/three.module.js';
+
+// just waiting for your beautiful creations!
